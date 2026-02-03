eca7c972e06fdb89e5846fa81ae58710d4c935e2
Bakame1 <marko@danet.one>
2026-02-03 15:22:50 +0100

    add home component

---
+++ src/app/app-home/app-home-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-home',
+  standalone: true,
+  imports: [],
+  template: `<p>Bienvenue sur ma home page !</p>`,
+  styles: ``
+})
+export class AppHomeComponent {}
